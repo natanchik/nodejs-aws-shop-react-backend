7493a078ef64e8208f799679596ba899a0be2f3f
Natalia Lebedeva <nataliadaveng@gmail.com>
2025-03-09 06:31:15 +0500

    feat: add importProduct function

---
+++ import-service/lambda/importProductsFile .ts
@@ -0,0 +1,51 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+const s3Client = new S3Client({});
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+  try {
+    const fileName = event.queryStringParameters?.name;
+
+    if (!fileName) {
+      return {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+        },
+        body: JSON.stringify({ message: 'File name is required' }),
+      };
+    }
+
+    const bucketName = 'rss-nodejs-aws-shop-react-backend-import-service';
+    const folderName = 'uploaded';
+    const key = `${folderName}/${fileName}`;
+
+    const command = new PutObjectCommand({
+      Bucket: bucketName,
+      Key: key,
+      ContentType: 'text/csv',
+    });
+
+    const signedUrl = await getSignedUrl(s3Client, command, {
+      expiresIn: 3600,
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: signedUrl,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify({ message: 'Internal server error' }),
+    };
+  }
+};
--- import-service/lib/import-service-stack.ts
+++ import-service/lib/import-service-stack.ts
@@ -1,16 +1,62 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class ImportServiceStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const importBucket = s3.Bucket.fromBucketName(
+      this,
+      'ImportBucket',
+      'rss-nodejs-aws-shop-react-backend-import-service',
+    );
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ImportServiceQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const importProductsFile = new lambda.Function(this, 'ImportProductsFile', {
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: 'importProductsFile.handler',
+      code: lambda.Code.fromAsset('lambda'),
+      environment: {
+        BUCKET_NAME: importBucket.bucketName,
+        UPLOADED_FOLDER: 'uploaded',
+      },
+    });
+
+    const bucketPolicy = new s3.BucketPolicy(this, 'ImportServiceBucketPolicy', {
+      bucket: importBucket,
+    });
+
+    bucketPolicy.document.addStatements(
+      new iam.PolicyStatement({
+        sid: 'Statement1',
+        effect: iam.Effect.ALLOW,
+        principals: [
+          new iam.ServicePrincipal('lambda.amazonaws.com'),
+          new iam.ArnPrincipal(importProductsFile.role?.roleArn || ''),
+        ],
+        actions: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject', 's3:ListBucket'],
+        resources: [importBucket.bucketArn, `${importBucket.bucketArn}/*`],
+      }),
+    );
+
+    importBucket.grantReadWrite(importProductsFile);
+
+    const api = new apigateway.RestApi(this, 'ImportApi', {
+      restApiName: 'Import Service',
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigateway.Cors.ALL_ORIGINS,
+        allowMethods: apigateway.Cors.ALL_METHODS,
+      },
+    });
+
+    const importResource = api.root.addResource('import');
+    importResource.addMethod('GET', new apigateway.LambdaIntegration(importProductsFile), {
+      requestParameters: {
+        'method.request.querystring.name': true,
+      },
+    });
   }
 }
